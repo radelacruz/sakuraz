65e6fb676165b381215ae473adba9cd6b1d91d01
Razelle Ann Dela Cruz <razelleanndelacruz@gmail.com>
2019-01-21 09:32:20 +0800

    ecom

--- app/views/error.php
+++ app/views/error.php
@@ -4,8 +4,8 @@ function get_page_content() {
 	// global $conn;
 ?>
 	<div class="container">
-		<!-- <h1>You don't have access to view this page</h1> -->
-		<h1>Alis!!!</h1>
+		<h1>You don't have access to view this page</h1>
+		<!-- <h1>Alis!!!</h1> -->
 		<a href="./home.php">Return Home</a>
 	</div> <!-- end container -->
 
