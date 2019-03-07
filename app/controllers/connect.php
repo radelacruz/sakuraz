cc4eed8b6d32bd13e6e447ddb1dfbd9056487e99
razelle ann dela cruz <razelleanndelacruz@gmail.com>
2019-03-07 13:43:45 +0800

    update

--- app/controllers/connect.php
+++ app/controllers/connect.php
@@ -1,8 +1,8 @@
 <?php
-	$host = 'https://databases-auth.000webhost.com';
-	$username = 'id8908514_annadriano';
-	$password = 'incorrect';
-	$dbname = 'id8908514_sakuraz_db';
+	$host = 'sql12.freemysqlhosting.net';
+	$username = 'sql12282203';
+	$password = 'wD8veeMY1F';
+	$dbname = 'sql12282203';
 
 	// Server: sql12.freemysqlhosting.net
 	// Name: sql12278905
